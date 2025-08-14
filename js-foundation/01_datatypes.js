0756a349a52ecc180180a885ee7a767e8f6e443b
Sofiya Sindgi <sofiyasindgi@gmail.com>
2025-08-15 00:17:31 +0530

    add js foundations folder with datatypes file

---
+++ js-foundation/01_datatypes.js
@@ -0,0 +1,41 @@
+/*datatypes
+ String
+ Number
+ Boolean
+ BigInt
+
+ undefined
+ null
+
+ Object
+
+ Symbol
+*/
+//examples
+//var score = 102;  // old method
+
+let score = 103;// modern method
+let name = "safi"
+let islogged = false
+
+//objects
+let teatypes = ["lemon-tea", "orange-tea"]
+let user = { firstname: "amal", lastname: "gupta" }
+let getscore = score;
+
+console.log(getscore);
+
+//assigning values
+let gameName = "spiderman";
+gameName = "batman"; //no need to write let again
+
+console.log(gameName);
+
+const userName = "spiderman";
+userName = "batman";
+
+console.log(userName);//this will give error bcz const means the variable binding is immutable — you cannot reassign it to a new value.
+
+
+
+
